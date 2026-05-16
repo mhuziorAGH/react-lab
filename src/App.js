071ac906a18b246b483085e59e222288816fdffa
Student <student@d17-426a-pc-13.ipa.ii.agh.edu.pl>
2026-05-16 11:26:20 +0200

    Dodana reaktywność

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
 
     function handleChange(event) {
-
-        console.log(event.target.value);
-
+        setEmail(event.target.value);
     }
+        let result = '';
+        if (email.length <= 5) {
+            result = "Mega krótkie";
+        } else if (email.length > 5 && email.length < 10){
+            result = "JestgGit";
+        } else{
+            result = "Mega długie";
+        }
   return (
     <div>
         <h1>System do zapisów na zajęcia</h1>
         <h2>Twój e-mail to {email}</h2>
-        <input type="text" onChange={handleChange}/>
+        {email.length > 0 && <div>{result}</div>}
+        <input type="text" value={email} onChange={handleChange}/>
     </div>
   );
 }
