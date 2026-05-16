5a34e7b1aef5213512cb51665d16bf14bf27bd2f
Student <student@d17-426a-pc-13.ipa.ii.agh.edu.pl>
2026-05-16 11:45:16 +0200

    Dodany pierwszys przycisk

--- src/App.js
+++ src/App.js
@@ -8,20 +8,25 @@ function App() {
     function handleChange(event) {
         setEmail(event.target.value);
     }
-        let result = '';
-        if (email.length <= 5) {
-            result = "Mega krótkie";
-        } else if (email.length > 5 && email.length < 10){
-            result = "JestgGit";
-        } else{
-            result = "Mega długie";
-        }
+    function hendleClick(){
+        alert(email);
+    }
+
+    let result = '';
+    if (email.length <= 5) {
+        result = "Mega krótkie";
+    } else if (email.length > 5 && email.length < 10){
+        result = "JestgGit";
+    } else{
+        result = "Mega długie";
+    }
   return (
     <div>
         <h1>System do zapisów na zajęcia</h1>
         <h2>Twój e-mail to {email}</h2>
         {email.length > 0 && <div>{result}</div>}
         <input type="text" value={email} onChange={handleChange}/>
+        <button onClick={hendleClick}>Wyślij eMail</button>
     </div>
   );
 }
